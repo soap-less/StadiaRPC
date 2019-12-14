423e64e966b61a6298ce73a39ecbfdafe92967a9
soap <david.parada.w@gmail.com>
2019-12-13 16:22:23 -0800

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,1 @@
-const url = chrome.runtime.getURL('games.json');
-let games = {};
-fetch(url).then(resp => resp.json()).then(json => games = json);
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    Object.keys(games).forEach(gameName => {
-        let game = games[gameName];
-        if (game["play"] === tab.url) {
-            alert(tab.url)
-        }
-    });
-});
+//Initial Commit.
