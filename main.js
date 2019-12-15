a203b2a2884b24468e256982f31e3d7481d4a7b2
soap <david.parada.w@gmail.com>
2019-12-15 13:24:02 -0800

    Popup link changed, better support for long game names

--- main.js
+++ main.js
@@ -6,68 +6,94 @@ let games = {};
     games = await response.json();
 })();
 
-// Register Presence
+//Register Presence
 chrome.runtime.sendMessage(extensionId, { mode: 'active' }, function (response) {
     console.log('Presence registred', response)
 });
 
-// Wait for presence Requests
+//Wait for presence Requests
 chrome.runtime.onMessage.addListener(function (info, sender, sendResponse) {
     console.log('Presence requested', info);
     sendResponse(getPresence());
 });
 
-//Why these need to be declared globally, I'll never know.
+//Set default options
+let stateDisplay = "";  
 let largeImgTxt = "";
 let smallImgTxt = "Online";
 let largeImg = "stadialogosquare";
 let smallImg = "online";
 let time = Date.now();
-let prevStateDisplay = "Unknown Game";
+let detailDisplay = "Playing an Unknown Game";
+let prevDetailDisplay = "Playing an Unknown Game";
+let prevStateDisplay = ""
 
-// Return Presence   
+//Return presence   
 function getPresence() {
     tabURL = location.href;
 
-    largeImgTxt = "";
-    smallImgTxt = "Online";
-    largeImg = "stadialogosquare";
-    smallImg = "online";
-
-
-    //Set details
-    let stateDisplay = "Unknown Game";
+    //Updates the options
     if (tabURL === "https://stadia.google.com/home") {
-        stateDisplay = "Chilling in Home";
+        detailDisplay = "Chilling in Home";
         largeImgTxt = "On the Home Page";
     } else if (tabURL.startsWith("https://stadia.google.com/store")) {
-        stateDisplay = "Browsing the Store";
+        detailDisplay = "Browsing the Store";
         largeImgTxt = "Looking for Something New";
+
+        //Displays what game is being viewed on the store
+        Object.keys(games).forEach(gameName => {
+            let game = games[gameName];
+            if (game["store"] === tabURL) {
+                stateDisplay = "Checking out " + gameName;
+            }
+        });
+
     } else {
         Object.keys(games).forEach(gameName => {
             let game = games[gameName];
             if (game["play"] === tabURL) {
-                stateDisplay = "Playing " + gameName;
+                detailDisplay = "Playing " + gameName;
                 largeImgTxt = gameName;
                 smallImgTxt = "on Stadia"
-                if (game["hasIcon"] === true) {
+                if (game["hasIcon"]) {
                     largeImg = game["aliases"][0];
-                    smallImg = "stadialogosquare"
+                    smallImg = "stadialogosquare";
+                }
+                //Way too much work for seperating names onto different lines, may have to optimize later
+                if (gameName.includes(":") && gameName.length + 8 > 25) {
+                    for (i = 0; i < gameName.length; i++) {
+                        if (gameName[i] == ":") {
+                            detailDisplay = "Playing " + gameName.slice(0, i + 1);
+                            stateDisplay = gameName.slice(i + 2);
+                        }
+                    }
+                } else if (gameName.length + 8 > 25 && gameName.length <= 25) {
+                    detailDisplay = "Playing";
+                    stateDisplay = gameName;
+                } else if (gameName.length + 8 > 25) {
+                    for (i = gameName.length - 1; i >= 0; i--) {
+                        if (gameName[i] == " " && gameName.slice(0, i).length + 8 < 25) {
+                            detailDisplay = "Playing" + gameName.slice(0, i);
+                            stateDisplay = gameName.slice(i + 1);
+                        }
+                    }
                 }
             }
         });
     } 
 
-    //Check to see if presence has changed
-    if (prevStateDisplay !== stateDisplay) {
+    //Check to see if presence has changed, resets time if so
+    if (prevDetailDisplay !== detailDisplay || prevStateDisplay !== stateDisplay) {
         time = Date.now();
     }
+    prevDetailDisplay = detailDisplay;
     prevStateDisplay = stateDisplay;
 
     return {
         clientId: '648430151390199818',
         presence: {
-            details: stateDisplay,
+            details: detailDisplay,
+            state: stateDisplay,
             startTimestamp: time,
             instance: true,
             largeImageKey: largeImg,
