140be9b68b8aa0c9d64dc5b2e1b5bf68ac4e6aba
soap <david.parada.w@gmail.com>
2019-12-17 19:18:22 -0800

    Version checking improved, added version to popup
    Version checking no longer just checks for a mismatch, but instead makes sure there that the version on the application is lower than the live version.

--- background.js
+++ background.js
@@ -1,11 +1,3 @@
-let currentVersion = ""
-
-async function getVersion() {
-    let response = await fetch("https://raw.githubusercontent.com/soap-less/StadiaRPC/master/manifest.json");
-    let json = await response.json();
-    currentVersion = json["version"];
-};
-
 chrome.runtime.onMessageExternal.addListener(function (request, sender, sendResponse) {
     chrome.tabs.sendMessage(request.tab, request.info, function (response) {
         sendResponse(response);
@@ -13,11 +5,39 @@ chrome.runtime.onMessageExternal.addListener(function (request, sender, sendResp
     return true;
 });
 
+let liveVersion = ""
+async function getLiveVersion() {
+    let response = await fetch("https://raw.githubusercontent.com/soap-less/StadiaRPC/master/manifest.json");
+    let json = await response.json();
+    liveVersion = json["version"];
+};
+
+//Shout out to StackOverflow for letting me know .split exists. Splits the version into a list of 3 numbers and then compares them.
 chrome.runtime.onStartup.addListener(function() {
-    getVersion().then(() => {
-        let manifestVersion = chrome.runtime.getManifest()["version"];
-        if (manifestVersion !== currentVersion) {
-            chrome.browserAction.setBadgeText({text: "!"});
+    getLiveVersion().then(() => {
+        let localVersion = chrome.runtime.getManifest()["version"];
+        localVersion = localVersion.split('.');
+        liveVersion = liveVersion.split('.');
+
+        for (i = 0; i < localVersion.length; i++){
+            localVersion[i] = parseInt(localVersion[i])
+        }
+
+        for (i = 0; i < liveVersion.length; i++){
+            liveVersion[i] = parseInt(liveVersion[i])
+        }
+
+        alert(localVersion.toString());
+        alert(liveVersion.toString());
+
+        for (i = 0; i < liveVersion.length; i++){
+            if (liveVersion[i] > localVersion[i]) {
+                alert("hi")
+                chrome.browserAction.setBadgeText({text: "!"});
+                break;
+            } else if (i === liveVersion.length - 1) {
+                chrome.browserAction.setBadgeText({text: ""});
+            }
         }
     });
 });
