4d7dbc81139454a76d0ca338cb87505df07b1035
soap <david.parada.w@gmail.com>
2019-12-18 14:30:47 -0800

    Pop-up properly displays newest version.

--- popup.js
+++ popup.js
@@ -12,5 +12,6 @@ document.addEventListener('DOMContentLoaded', function () {
             document.body.insertBefore(bannerDiv, document.getElementById("header"));
         }
     });
+    document.getElementById("signature").innerHTML = "soapless (" + chrome.runtime.getManifest()["version"] + ")";
 });
 
