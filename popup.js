8c105fa7490a3e6383fc1980bc23b9ad6ed70045
soap <david.parada.w@gmail.com>
2019-12-18 18:41:27 -0800

    new settings page
    missing transition

--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    //Set version #
+    document.getElementById("signature").innerHTML = "soapless (" + chrome.runtime.getManifest()["version"] + ")";
+
     //Adds a banner when out of date
     chrome.browserAction.getBadgeText({}, function(badgeText) {
         if (badgeText === "!") {
@@ -9,9 +13,28 @@ document.addEventListener('DOMContentLoaded', function () {
             bannerText.setAttribute("href", "https://chrome.google.com/webstore/detail/stadiarpc/dmhhgpkmilabgjpdbkinimkihdiobljg?hl=en&gl=US")
             bannerText.setAttribute("target", "_newtab");
             bannerDiv.appendChild(bannerText);
-            document.body.insertBefore(bannerDiv, document.getElementById("header"));
+            document.body.insertBefore(bannerDiv, document.getElementById("settingsPage"));
         }
     });
-    document.getElementById("signature").innerHTML = "soapless (" + chrome.runtime.getManifest()["version"] + ")";
-});
 
+    document.getElementById("settings").addEventListener("click", function() {
+        let mainPage = document.getElementById("mainPage");
+        let settingsPage = document.getElementById("settingsPage");
+        let settingsIcon = document.getElementById("settings")
+
+        if(settingsPage.getAttribute("style") === "height: 0") {
+            settingsIcon.setAttribute("src", "/assets/arrow.png")
+            settingsIcon.setAttribute("class", "imgLink left")
+
+            mainPage.setAttribute("style", "height: 0");
+            settingsPage.setAttribute("style", "");
+
+        } else {
+            settingsIcon.setAttribute("src", "/assets/settings.png")
+            settingsIcon.setAttribute("class", "imgLink right")
+
+            settingsPage.setAttribute("style", "height: 0");
+            mainPage.setAttribute("style", "");
+        }
+    });
+});
